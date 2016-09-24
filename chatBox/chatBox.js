2d851315df1096daebbcf0f68431e8d2cb4515d8
lidong <lid@qipengkeji.com>
2016-09-24 20:24:03 +0800

    service

---
+++ chatBox/chatBox.js
@@ -0,0 +1,94 @@
+/**
+ * Created by Administrator on 2016/9/23.
+ */
+$(function(){
+    var data;
+    window.onload=function(){
+        getAssetInfo();
+    };
+
+    $('.chatOver').click(function(){
+        chatOver();
+    });
+    //填写页面资产信息
+    var rendAssetInfo = function (data) {
+
+        var _account=data.account,
+            _commissionRate=_account.commissionRate,
+            _CRMScale=_account.CRMScale,
+            _SubcontractManager=_account.SubcontractManager,
+
+            _property=data.property,
+            _totalAssets=_property.totalAssets,
+            _debtAssets=_property.debtAssets,
+            _idleAssets=_property.idleAssets,
+            _netAssets=_property.netAssets,
+
+            _goldAsset=data.goldAssets,
+            _productName1=_goldAsset[0].productName,
+            _productTypeName1=_goldAsset[0].productTypeName,
+            _productClassifyName1=_goldAsset[0].productClassifyName,
+            _productTime1=_goldAsset[0].productTime,
+            _productName2=_goldAsset[1].productName,
+            _productTypeName2=_goldAsset[1].productTypeName,
+            _productClassifyName2=_goldAsset[1].productClassifyName,
+            _productTime2=_goldAsset[1].productTime,
+
+            _serviceAsserts=data.serviceAsserts,
+            _serviceName=_serviceAsserts.serviceName,
+            _collectionManner=_serviceAsserts.collectionManner,
+            _name=_serviceAsserts.name,
+            _startTime=_serviceAsserts.startTime,
+            _endTime=_serviceAsserts.endTime;
+
+            $('.assetInfo').append(''+
+                '<div>小方13716356666,31319212|13716356666（北京）   |   客户号：31319212</div> <div>北京市海淀区龙翔路8号楼</div>' +
+                ' <div>【账户】 交易佣金率：'+_commissionRate+'   |   CRM等级： '+_CRMScale+'  |   分包理财经理：'+_SubcontractManager+'</div>' +
+                ' <div>【资产】 普通总资产：'+_totalAssets+'   |   股基权债资产：'+_debtAssets+'   |   空闲资金：'+_idleAssets+'   |   两融净资产：'+_netAssets+'</div>' +
+                ' <div>【金产】 '+_productName1+' ( 权益类公募 ,  风险1 ,  20160321 )</div>' +
+                ' <div>【服产】 '+_serviceName+'</div>'
+            );
+    };
+    //结束会话
+    function chatOver (){
+        // 正式URL
+        //var _url='http://10.150.33.168:8080/ifc-kefu-mock-webapp/allUser/change?sessionId=10001';
+        // 测试URL
+         var _url = '../testdata/tsconfig.json';
+        $.ajax({
+            url: _url,
+            type: "GET",
+            dataType: "JSON",
+            success: function (d) {
+                data= d;
+                // 开始填充页面元素
+                alert(data[0].sessionId)
+            },
+            error: function () {
+                // console.log("error");
+                alert("网络错误，请刷新重试！");
+            }
+        });
+    }
+    //获取资产信息
+    function getAssetInfo(){
+        // 正式URL
+        //var _url='http://10.150.33.168:8080/ifc-kefu-mock-webapp/allUser/getJson';
+        // 测试URL
+        var _url = '../testdata/tsconfig.json';
+        $.ajax({
+            url: _url,
+            type: "GET",
+            dataType: "JSON",
+            success: function (d) {
+                data= d;
+                // 开始填充页面元素
+                rendAssetInfo(data);
+            },
+            error: function () {
+                // console.log("error");
+                alert("网络错误，请刷新重试！");
+            }
+        });
+    }
+});
---
+++ js/index.js
@@ -0,0 +1,302 @@
+/**
+ * Created by Administrator on 2016/9/21.
+ */
+$(function(){
+    var _response;
+    window.onload=function(){
+        loadUndoUserData(_response);
+        if ($('.issueNumber').html==""){
+            $(this).hide()
+        }
+    };
+
+    //待处理、全部任务的按钮点击
+    $('.total').on('click',function(){
+        $('.undoList').empty().hide();
+        $('.totalList').empty().show();
+        $('.undoPageContainer').hide();
+        $('.totalPageContainer').show();
+        loadDefaultUserData();
+    });
+    $('.undo').on('click',function(){
+        $('.undoList').empty().show();
+        $('.totalList').empty().hide();
+        $('.undoPageContainer').show();
+        $('.totalPageContainer').hide();
+        loadUndoUserData(_response)
+    });
+    $('.undo,.total').on('click',function(){
+        $('.chatboxSend').toggle();
+    });
+    //对话任务待处理和全部的切换
+    $('.btn_group button').click(function(){
+        $(this).addClass('btn_selected').siblings().removeClass('btn_selected')
+    });
+    //左侧导航栏按钮颜色切换
+    $('.menu li').click(function(){
+        $(this).addClass('li_selected').siblings().removeClass('li_selected')
+    });
+    //对话任务列表选定样式切换
+    $('.totalList,.undoList').on('click','.list_Item',function(){
+        $(this).addClass('Item_selected').siblings().removeClass('Item_selected');
+        $('.chatIframe').css({"visibility":"visible"})
+    });
+    //高级搜索显示与隐藏
+    $('.searchSettingOpen').click(function(e){
+        $(".searchSetting").css({"visibility":"visible"});
+        $(document).one("click", function(){
+            $(".searchSetting").css({"visibility":"hidden"});
+        });
+        e.stopPropagation();
+    });
+    $(".searchSetting").on("click", function(e){
+        e.stopPropagation();
+    });
+    //待处理分页按钮功能样式
+    $('.undoPage li').on('click',function(){
+        var index =$(this).index();
+
+        loadPageUndoUserData(index);
+    });
+    //
+    //全部分页按钮功能样式
+    $('.totalPage li').on('click',function(){
+        var index =$(this).index();
+
+        loadPageTotalUserData(index);
+    });
+    //搜索功能样式
+    $('.searchBegin').click(function(){
+        if ($('.listSearch').val()!=""){
+
+        }
+        var S="phone="+"13028316633";
+        $('.totalList').empty();
+        _response="";
+        Search(S);
+    });
+    //抢单功能
+    $('.totalList').on('click','.qiangdan',function(e){
+        $(this).hide().siblings('._status').html("已领取");
+        //抢单的sessionId和kfnumber
+        qd=[$(this).closest('.list_Item').find('#sessionId').html(),$(this).closest('.list_Item').find('#kfNumber').html()];
+        //点击抢单更新数据
+        loadqiangdan(qd);
+        //更新待处理数据，将新单加入个户列表
+        loadUndoUserData();
+        //更新全部列表数据
+        rendFormDataUndo();
+        e.stopPropagation()
+    });
+    // 填充全部任务列表动态数据
+    var rendFormDataTotal = function (_response) {
+        for(var i=0;i<_response.length;i++){
+                var _sessionId= _response[i].sessionId,
+                    _phone= _response[i].phone,
+                    _startTime= _response[i].startTime,
+                    _endTime= _response[i].endTime,
+                    _status= _response[i].status,
+                    qiangdan;
+                    _kfNumber= _response[i].kfNumber,
+                    _chatLog= _response[i].chatLog,
+                    _type= _chatLog.type,
+                    _content=_chatLog.content,
+                    _time=_chatLog.time;
+                    qiangdan=getStatus(_status);
+
+            $('.totalList').append(
+                '<li class="list_Item"> ' +
+                ' <div class="fl"> ' +
+                '<div class="fl" id="sessionId">'+_sessionId+' </div>' +
+                '<span>(app咨询)</span> ' +
+                '<div><span  class="status" id="endTime">'+_endTime+' 18:50</span></div>' +
+                ' <div>来源:app</div> <div id="kfNumber">'+_kfNumber+' </div>' +
+                ' </div> <div class="fr">' +
+                ' <div>09-08</div>' +
+                ' <span class="_status">'+qiangdan+'</span>' +
+                '<buttton class="qiangdan">抢单</buttton> </div>' +
+                '</li>'
+            );
+            if (qiangdan=="待领取"){
+                $('._status').css({"color":"blue"})
+                $('.qiangdan').show()
+            }
+
+        }
+    };
+    //填充待处理任务列表动态数据
+    var rendFormDataUndo = function (_response,newTask) {
+        $('.undoList').prepend(newTask);
+        for (var i = 0; i < _response.length; i++) {
+            var _sessionId = _response[i].sessionId,
+                _phone = _response[i].phone,
+                _startTime = _response[i].startTime,
+                _endTime = _response[i].endTime,
+                _status = _response[i].status,
+            _kfNumber = _response[i].kfNumber,
+                _chatLog = _response[i].chatLog,
+                _type = _chatLog.type,
+                _content = _chatLog.content,
+                _time = _chatLog.time;
+
+            $('.undoList').append(
+                '<li class="list_Item"> ' +
+                '<input type="checkbox" class="editList">' +
+                ' <div class="fl"> ' +
+                '<div class="fl" id="sessionId">' + _sessionId + ' </div>' +
+                '<span>(app咨询)</span> ' +
+                '<div><span  class="status" id="endTime">' + _endTime + ' 18:50</span></div>' +
+                ' <div>来源:app</div> <div id="kfNumber">' + _kfNumber + ' </div>' +
+                ' </div> <div class="fr">' +
+                ' <div>09-08</div>' +
+                '</li>'
+            );
+        }
+
+        $('.issueNumber').html($('.undoList').find('.list_Item').length)
+
+    };
+    // 加载全部任务信息
+    var loadDefaultUserData = function () {
+        $('.totalList').empty();
+       // 正式URL
+       var _url='http://localhost:8080/ifc-kefu-mock-webapp/allUser/index';
+        // 测试URL
+       // var _url = './testdata/tsconfig.json';
+            $.ajax({
+                url: _url,
+                type: "GET",
+                dataType: "JSON",
+                success: function (d) {
+                    _response= d;
+                    // 开始填充页面元素
+                    rendFormDataTotal(_response);
+                },
+                error: function () {
+                    // console.log("error");
+                    alert("网络错误，请刷新重试！");
+                }
+            });
+        };
+    //加载待处理列表
+    var loadUndoUserData =function(){
+        $('.undoList').empty();
+        var _url="http://localhost:8080/ifc-kefu-mock-webapp/allUser/kfIndex?kfNumber=95571";
+        // 测试URL
+        //var _url = './testdata/tsconfig.json';
+        $.ajax({
+            url: _url,
+            type: "GET",
+            dataType: "JSON",
+            success: function (d) {
+                _response= d;
+                // 开始填充页面元素
+                rendFormDataUndo(_response);
+            },
+            error: function () {
+                // console.log("error");
+                alert("网络错误，请刷新重试！");
+            }
+        });
+    };
+    //加载待处理任务分页
+    var loadPageUndoUserData =function (index){
+        $('.undoPage li').eq(index).addClass('pageSelected').siblings().removeClass('pageSelected');
+        $('.undoList').empty();
+        var _url='localhost:8080/ifc-kefu-mock-webapp/allUser/kfIndex?pageIndex='+index;
+        $.ajax({
+            url: _url,
+            type: "GET",
+            dataType: "JSON",
+            success: function (d) {
+                _response= d;
+                // 开始填充页面元素
+                rendFormDataUndo(_response);
+            },
+            error: function () {
+                // console.log("error");
+                alert("网络错误，请刷新重试！");
+            }
+        });
+
+    };
+    //加载全部任务分页
+    var loadPageTotalUserData =function(index){
+        $('.totalPage li').eq(index).addClass('pageSelected').siblings().removeClass('pageSelected');
+        $('.totalList').empty();
+        var _url='localhost:8080/ifc-kefu-mock-webapp/allUser/index?pageIndex='+index;
+        $.ajax({
+            url: _url,
+            type: "GET",
+            dataType: "JSON",
+            success: function (d) {
+                _response= d;
+                // 开始填充页面元素
+                rendFormDataTotal(_response);
+            },
+            error: function () {
+                // console.log("error");
+                alert("网络错误，请刷新重试！");
+            }
+        });
+    };
+    //改变抢单状态
+    var loadqiangdan=function(qd){
+        // 正式URL
+        var _url='localhost:8080/ifc-kefu-mock-webapp/allUser/changeStatus?sessionId='+qd[0]+'&kfNumber='+qd[1];
+        alert(_url)
+        $.ajax({
+            url: _url,
+            type: "GET",
+            dataType: "JSON",
+            success: function (d) {
+                _response= d;
+                alert("成功")
+            },
+            error: function () {
+                alert("网络错误，请刷新重试！");
+            }
+        });
+
+    };
+    //加载搜索
+
+    //加载搜索
+    //
+    //var Search=function(search){
+    //    // 正式URL
+    //    var _url='localhost:8080/ifc-kefu-mock-webapp/allUser/index?'+search;
+    //    // 测试URL
+    //    //var _url = './testdata/tsconfig.json';
+    //    $.ajax({
+    //        url: _url,
+    //        type: "GET",
+    //        dataType: "JSON",
+    //        success: function (d) {
+    //            _response= d;
+    //            // 开始填充页面元素
+    //            rendFormDataTotal(_response);
+    //        },
+    //        error: function () {
+    //            // console.log("error");
+    //            alert("网络错误，请刷新重试！");
+    //        }
+    //    });
+    //};
+    var getStatus=function(_status){
+        var qiangdan;
+        if(_status==0){
+            qiangdan="待领取";
+        }
+        if (_status==1){
+            qiangdan="已领取";
+        }
+        if(_status==2){
+            qiangdan="已关闭";
+        }
+        return qiangdan;
+    }
+    });
+
+
+
