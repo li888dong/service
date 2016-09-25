6eaae768273116d623cc119accd88eff3a2027a5
lidong <lid@qipengkeji.com>
2016-09-25 13:55:37 +0800

    service

--- chatBox/chatBox.js
+++ chatBox/chatBox.js
@@ -2,16 +2,17 @@
  * Created by Administrator on 2016/9/23.
  */
 $(function(){
-    var data;
-
     var sessionId=getUrlParam('sessionId');
+    var msg;
     getAssetInfo();
     chatStart(sessionId);
     $('.chatOver').click(function(){
         chatOver();
     });
+
+
     //填写页面资产信息
-    var rendAssetInfo = function (data) {
+     function rendAssetInfo(data) {
         var _account=data.account,
             _commissionRate=_account.commissionRate,
             _CRMScale=_account.CRMScale,
@@ -47,7 +48,26 @@ $(function(){
                 ' <div>【金产】 '+_productName1+' ( 权益类公募 ,  风险1 ,  20160321 )</div>' +
                 ' <div>【服产】 '+_serviceName+'</div>'
             );
-    };
+    }
+    //填写聊天框内容信息
+    function rendMessageReceived(data) {
+        $('.chatboxReceived').empty();
+        for (var i=0;i<data.length;i++){
+            var _type=data[i].type,
+                _content=data[i].content,
+                _time=data[i].time;
+
+            $('.chatboxReceived').append('' +
+                '<div class="chatHistoryContainer">' +
+                '<span class="chatHistory">'+_type+'</span> ' +'<br>'+
+                '<span class="chatHistory">'+_content+'</span>' +'<br>'+
+                '<p> '+_time+'</p>' +
+                '</div> '
+            )
+        }
+
+
+    }
     //开始会话
     function chatStart (arg){
         // 正式URL
@@ -58,15 +78,27 @@ $(function(){
             url: _url,
             type: "GET",
             dataType: "JSON",
+            timeout:1000,
             success: function (d) {
-                data= d;
                 // 开始填充页面元素
-                alert(data[0].content)
+                rendMessageReceived(d);
             },
             error: function () {
                 // console.log("error");
                 alert("网络错误，请刷新重试！");
+            },
+            complete : function(){
+                //一秒刷新一次聊天内容
+                setTimeout(function(){chatStart(sessionId);}, 1000);
             }
+        });
+
+    }
+    //发送信息
+    function sendMessage(msg) {
+        $.ajax({
+            type: 'get',
+            url: 'http://localhost:8080/ifc-kefu-mock-webapp/allUser/saveMessage?sessionId='+sessionId+'&Message='+msg
         });
     }
     //结束会话
@@ -80,9 +112,8 @@ $(function(){
             type: "GET",
             dataType: "JSON",
             success: function (d) {
-                data= d;
                 // 开始填充页面元素
-                alert(data[0].sessionId)
+                alert(d[0].sessionId)
             },
             error: function () {
                 // console.log("error");
@@ -118,4 +149,24 @@ $(function(){
         if (r != null) return unescape(r[2]);
         return null; //返回参数值
     }
+    //scroll美化
+    $('.chatboxReceived').niceScroll({
+        cursorcolor: "#ccc",//#CC0071 光标颜色
+        cursoropacitymax: 1, //改变不透明度非常光标处于活动状态（scrollabar“可见”状态），范围从1到0
+        touchbehavior: false, //使光标拖动滚动像在台式电脑触摸设备
+        cursorwidth: "5px", //像素光标的宽度
+        cursorborder: "0", // 	游标边框css定义
+        cursorborderradius: "5px",//以像素为光标边界半径
+        autohidemode: false //是否隐藏滚动条
+    });
+    //enter键发送消息
+    $('.sendMsg').on('keyup',function(event){
+        if (event.keyCode == "13") {//keyCode=13是回车键
+            sendMessage(msg);
+            $('.sendMsg').val("");
+        }else{
+            msg =$('.sendMsg').val();
+        }
+        event.stopPropagation();
+    });
 });
